67014a13507d2251cf5a66d48953985b133b8f5b
Rimuruu <antoine.renciot@gmail.com>
2020-07-22 11:14:17 +0200

    fix bug

---
+++ build/AnimatedBG-13c6404f.js
@@ -0,0 +1,2 @@
+import{S as s,i as a,s as e,e as t,a as i,b as n,n as o,d}from"./main-6eb4426a.js";function r(s){let a;return{c(){a=t("div"),i(a,"class","video p-0 shadow-p animatedBG svelte-8jvm2s")},m(s,e){n(s,a,e)},p:o,i:o,o:o,d(s){s&&d(a)}}}export default class extends s{constructor(s){super(),a(this,s,null,r,e,{})}}
+//# sourceMappingURL=AnimatedBG-13c6404f.js.map
--- build/main.js
+++ build/main.js
@@ -1,4 +1,2 @@
-
-(function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(window.document);
-export { k as default } from './main-eb171596.js';
+export{h as default}from"./main-6eb4426a.js";
 //# sourceMappingURL=main.js.map
