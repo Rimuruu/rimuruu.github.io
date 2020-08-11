0daa4eca98f39b9072dc957308fb5eae6b6cdc6d
Rimuruu <antoine.renciot@gmail.com>
2020-08-11 18:50:47 +0200

    minesweeper

---
+++ build/AnimatedBG-b2d71c01.js
@@ -0,0 +1,2 @@
+import{S as s,i as a,s as e,e as t,a as d,b as i,n,d as o}from"./main-6964d8f9.js";function r(s){let a;return{c(){a=t("div"),d(a,"class","video p-0 shadow-p animatedBG svelte-8jvm2s")},m(s,e){i(s,a,e)},p:n,i:n,o:n,d(s){s&&o(a)}}}export default class extends s{constructor(s){super(),a(this,s,null,r,e,{})}}
+//# sourceMappingURL=AnimatedBG-b2d71c01.js.map
